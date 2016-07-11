0e03910566ce232b43aafafd5fbcb38416688f57
Richie Lenninger <rlenninger@me.com>
2016-07-11 13:24:21 -0700

    Cleaned up code

--- practice/lastjoin.py
+++ practice/lastjoin.py
@@ -6,18 +6,16 @@
 word_list = (input('Please enter words separated by a space. ')).split()
 
 count = len(word_list)
+
 if count == 1:
-    word_string = str(word_list)
+    word_string = word_list[0]
+
 elif count == 2:
     word_string = ' and '.join(word_list)
+
 else:
-    # word_string.join(word_list[0:-1]
-    #
-    # temp = word_list[-1]
-    # word_string = word_string + ', and ' + temp
     word_string = ', '.join(word_list[0:-1])
     word_string = word_string + ', and ' + word_list[-1]
-
 
 # output
 print(word_string)
