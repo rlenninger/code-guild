c5f0134fca26324a06f2e0acfbcd18c8f946a902
Richie Lenninger <rlenninger@me.com>
2016-07-11 16:00:34 -0700

    finalizing code

--- practice/pig-latin.py
+++ practice/pig-latin.py
@@ -4,8 +4,6 @@
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
 # input
-
-# word_to_translate = list(input('Please type a single word to translate: '))
 word_to_translate = input('Please give me a word to translate into pig latin: ')
 word_to_translate_lower = word_to_translate.lower()
 word_to_translate_list = word_to_translate_lower.strip()
@@ -16,7 +14,7 @@ if word_to_translate_list[0] in VOWELS:
     translated_word = '' .join(word_to_translate_list) + 'yay'
 
 else:
-    translated_word = '' .join(word_to_translate_list[1:-1]) + 'ay' + '' .join(word_to_translate_list[0])
+    translated_word = '' .join(word_to_translate_list[1:]) + '' .join(word_to_translate_list[0]) + 'ay'
 
 # output
 print('The pig latin version will be: ' + translated_word)
