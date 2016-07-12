db012cb3a39e9a274b37dc25d4369ab937b7d7d5
Richie Lenninger <rlenninger@me.com>
2016-07-12 15:52:40 -0700

    renamed variables, cleaned up code

--- practice/wallpainting.py
+++ practice/wallpainting.py
@@ -5,14 +5,16 @@ Allows for multiple rooms.
 """
 
 # setup, starting variables to call the loop
-PAINT_AREA = 400
+SQ_FT_PER_GALLON_OF_PAINT = 400
 another_wall = 'y'
-total_area_of_all_rooms = 0
+total_sq_ft_of_all_rooms = 0
 # input
+
 print('How much does a gallon of paint cost?')
-cost = float(input())
+cost_of_paint = float(input())
 
 # while loop for further input and maths
+
 while another_wall == 'y':
     print('What is your walls width?')
     wall_width = float(input())
@@ -21,21 +23,25 @@ while another_wall == 'y':
     wall_height = float(input())
 
     print('How many coats of paint?')
-    coats = float(input())
+    coats_of_paint = float(input())
 # math, calculates surface area that needs to be covered
-# & uses total_area_of_all_rooms as a running tally
-    surface_area = wall_width * wall_height
-    surface_area_plus_coats = surface_area * coats
-    total_area_of_all_rooms = total_area_of_all_rooms + surface_area_plus_coats
+# & uses total_sq_ft_of_all_rooms as a running tally
+
+    sq_ft_of_wall = wall_width * wall_height
+    sq_ft_of_wall_times_coats_of_paint = sq_ft_of_wall * coats_of_paint
+    total_sq_ft_of_all_rooms = total_sq_ft_of_all_rooms + sq_ft_of_wall_times_coats_of_paint
 
     print('Do you have another wall to paint?')
     another_wall = input("y/n ")
 
 # transform, final math figures amount of paint needed
-paint_needed = total_area_of_all_rooms / PAINT_AREA
-total_cost = paint_needed * cost
+
+gallons_of_paint_needed = total_sq_ft_of_all_rooms / SQ_FT_PER_GALLON_OF_PAINT
+total_cost = gallons_of_paint_needed * cost_of_paint
 # used round() to round total cost to $X.xx
+
 total_cost_rounded = round(total_cost, 2)
 # output
-print('You will need to purchise ' + str(paint_needed) + ' gallons of paint.')
-print('Your total cost to will be, $' + str(float(total_cost_rounded)))
+
+print('You will need to purchise ' + str(gallons_of_paint_needed) + ' gallons of paint.')
+print('Your total cost to paint will be, $' + str(total_cost_rounded))
