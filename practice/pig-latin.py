dd3ab53b4982cf5b9ff1f3cbf9acd39127f29dc9
Richie Lenninger <rlenninger@me.com>
2016-07-12 15:44:43 -0700

    removed no-op's

--- practice/pig-latin.py
+++ practice/pig-latin.py
@@ -4,24 +4,21 @@
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
 def word_to_translate_from_user():
-    """Asks user for a word, converts to lower case, removes puncuation
-    and converts to a list.
+    """Asks user for a word, converts to lower case, removes puncuation.
     """
     word_to_translate = input('Please give me a word to translate into pig latin: ')
     word_to_translate_lower = word_to_translate.lower()
     word_to_translate_strip = word_to_translate_lower.strip('.?!')
-    word_to_translate_list = list(word_to_translate_strip)
-    return word_to_translate_list
+    return word_to_translate_strip
 
-
-def pig_latin_translator(word_to_translate_list):
+def pig_latin_translator(word_to_translate_strip):
     """Takes return from, word_to_translate_from_user and
     translates to pig latin.
     """
-    if word_to_translate_list[0] in VOWELS:
-        translated_word = '' .join(word_to_translate_list) + 'yay'
+    if word_to_translate_strip[0] in VOWELS:
+        translated_word = word_to_translate_strip + 'yay'
     else:
-        translated_word = '' .join(word_to_translate_list[1:]) + '' .join(word_to_translate_list[0]) + 'ay'
+        translated_word = word_to_translate_strip[1:] + word_to_translate_strip[0] + 'ay'
     return translated_word
 
 def main():
@@ -30,7 +27,7 @@ def main():
     """
     given_word = word_to_translate_from_user()
     output_text = pig_latin_translator(given_word)
-    output = str(output_text)
+    output = output_text
     print('The pig latin version will be: ' + output)
     return output
 
