e28bbf87bb2f8ba31a395047326120e169693f96
Richie Lenninger <rlenninger@me.com>
2016-07-12 14:56:14 -0700

    separated steps into def functions, fixed code and variable names to better reflect what they do

--- practice/pig-latin.py
+++ practice/pig-latin.py
@@ -3,18 +3,35 @@
 # setup
 VOWELS = ['a', 'e', 'i', 'o', 'u']
 
-# input
-word_to_translate = input('Please give me a word to translate into pig latin: ')
-word_to_translate_lower = word_to_translate.lower()
-word_to_translate_list = word_to_translate_lower.strip()
+def word_to_translate_from_user():
+    """Asks user for a word, converts to lower case, removes puncuation
+    and converts to a list.
+    """
+    word_to_translate = input('Please give me a word to translate into pig latin: ')
+    word_to_translate_lower = word_to_translate.lower()
+    word_to_translate_strip = word_to_translate_lower.strip('.?!')
+    word_to_translate_list = list(word_to_translate_strip)
+    return word_to_translate_list
 
-# transform
 
-if word_to_translate_list[0] in VOWELS:
-    translated_word = '' .join(word_to_translate_list) + 'yay'
+def pig_latin_translator(word_to_translate_list):
+    """Takes return from, word_to_translate_from_user and
+    translates to pig latin.
+    """
+    if word_to_translate_list[0] in VOWELS:
+        translated_word = '' .join(word_to_translate_list) + 'yay'
+    else:
+        translated_word = '' .join(word_to_translate_list[1:]) + '' .join(word_to_translate_list[0]) + 'ay'
+    return translated_word
 
-else:
-    translated_word = '' .join(word_to_translate_list[1:]) + '' .join(word_to_translate_list[0]) + 'ay'
+def main():
+    """Runs word_to_translate_from_user function, passes to
+    pig_latin_translator and prints output.
+    """
+    given_word = word_to_translate_from_user()
+    output_text = pig_latin_translator(given_word)
+    output = str(output_text)
+    print('The pig latin version will be: ' + output)
+    return output
 
-# output
-print('The pig latin version will be: ' + translated_word)
+main()
